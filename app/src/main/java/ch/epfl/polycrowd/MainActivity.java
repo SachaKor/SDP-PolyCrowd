e30b232dd979d13db7b348c8c0917b62e5d6afc5
Cedric Hoelzl <cedric.hoelzl@epfl.ch>
2020-03-04 12:30:10 +0100

    Added Basic EventEditView

--- app/src/main/java/ch/epfl/polycrowd/MainActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/MainActivity.java
@@ -3,12 +3,27 @@ package ch.epfl.polycrowd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void setViewEventEdit(View button) {
+        setContentView(R.layout.activity_event_edit);
+    }
+
+    public void sendEventSubmit(View button) {
+        final EditText evName = findViewById(R.id.EditEventName);
+        final String evnText = evName.getText().toString();
+
+
         setContentView(R.layout.activity_main);
     }
 }
