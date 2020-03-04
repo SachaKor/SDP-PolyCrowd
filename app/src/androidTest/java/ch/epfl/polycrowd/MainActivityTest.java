98cdc0194206c4e57f54540f4d7fa0b6fc2831de
SashaKor <alexandra.korukova@gmail.com>
2020-03-04 23:44:29 +0100

    sign up button in the main menu tested

--- app/src/androidTest/java/ch/epfl/polycrowd/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/polycrowd/MainActivityTest.java
@@ -8,7 +8,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.core.StringContains.containsString;
@@ -21,5 +23,11 @@ public class MainActivityTest {
     @Test
     public void testDisplaysHelloWorld() {
         onView(withId(R.id.helloText)).check(matches(withText(containsString("Hello World!"))));
+    }
+
+    @Test
+    public void testSignUpButtonClick() {
+        onView(withId(R.id.sign_up)).perform(click());
+        onView(withId(R.id.sign_up_button)).check(matches(isDisplayed()));
     }
 }
