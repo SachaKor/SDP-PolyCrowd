56f09eda71e0746f1a29d955ba2685539fc49131
batkem <demahomirdahk@gmail.com>
2020-03-05 16:44:37 +0100

    [Test] Added test coverage for interface getters

--- app/src/androidTest/java/ch/epfl/polycrowd/FirebaseInterfaceTest.java
+++ app/src/androidTest/java/ch/epfl/polycrowd/FirebaseInterfaceTest.java
@@ -6,6 +6,8 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -38,5 +40,39 @@ public class FirebaseInterfaceTest {
     public void checkArgsTestEmpty(){
         this.firebaseInterface.checkArgs("","");
     }
+    @Test(expected = IllegalArgumentException.class)
+    public void checkArgsTestEmpty2(){
+        this.firebaseInterface.checkArgs("abc","");
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void checkArgsTestNull2(){
+        this.firebaseInterface.checkArgs("abc",null);
+    }
+
+    @Test
+    public void checkDbRef(){
+        DatabaseReference ref = this.firebaseInterface.getDbRef(false);
+        assert(ref != null );
+        DatabaseReference ref2 = this.firebaseInterface.getDbRef(true);
+        assert(ref2!= null );
+    }
+    @Test
+    public void checkAuthRef(){
+        FirebaseAuth auth = this.firebaseInterface.getAuthInstance(false);
+        assert( auth != null );
+
+        FirebaseAuth auth2 = this.firebaseInterface.getAuthInstance(true);
+        assert(auth2 != null );
+    }
+
+    @Test
+    public void checkFireStoreRef(){
+        FirebaseFirestore fs = this.firebaseInterface.getFirestoreInstance(false);
+        assert(fs!= null );
+
+        FirebaseFirestore fs2 = this.firebaseInterface.getFirestoreInstance(true);
+        assert(fs2 != null );
+    }
+
 
 }
