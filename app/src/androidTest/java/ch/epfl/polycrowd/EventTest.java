a0ae4591f607080eb49ccfc7c9e8448742edad97
Cedric Hoelzl <cedric.hoelzl@epfl.ch>
2020-03-09 18:57:12 +0100

    Added tests cases for EventClass

--- app/src/androidTest/java/ch/epfl/polycrowd/EventTest.java
+++ app/src/androidTest/java/ch/epfl/polycrowd/EventTest.java
@@ -17,14 +17,15 @@ public class EventTest {
     private final Integer default_owner = 1;
     private final String default_name = "TestEvent";
     private final Event.EventType default_type = Event.EventType.OTHER;
+    private final Date default_sd = Date.newBuilder().setYear(1999).setMonth(8).setDay(8).build();
+    private final Date default_ed = Date.newBuilder().setYear(1999).setMonth(8).setDay(9).build();
+    private final TimeOfDay default_st = TimeOfDay.newBuilder().setHours(10).setMinutes(10).setSeconds(0).build();
+    private final TimeOfDay default_et = TimeOfDay.newBuilder().setHours(18).setMinutes(15).setSeconds(0).build();
 
     @Before
     public void init(){
-        Date sd = Date.newBuilder().setYear(1999).setMonth(8).setDay(8).build();
-        Date ed = Date.newBuilder().setYear(1999).setMonth(8).setDay(9).build();
-        TimeOfDay st = TimeOfDay.newBuilder().setHours(10).setMinutes(10).setSeconds(0).build();
-        TimeOfDay et = TimeOfDay.newBuilder().setHours(18).setMinutes(15).setSeconds(0).build();
-        e = new Event(default_owner,default_name, false, default_type, sd, ed, st, et,"None");
+        e = new Event(default_owner,default_name, false,
+                      default_type, default_sd, default_ed, default_st, default_et,"None");
 
     }
 
@@ -60,22 +61,39 @@ public class EventTest {
 
     @Test
     public void getSetStartD() {
+        assertEquals(default_sd, e.getStartD());
+        final Date new_date = Date.newBuilder().setYear(2020).setMonth(9).setDay(9).build();
+        e.setStartD(new_date);
+        assertEquals(new_date, e.getStartD());
     }
 
     @Test
     public void getSetEndD() {
+        assertEquals(default_ed, e.getEndD());
+        final Date new_date = Date.newBuilder().setYear(2020).setMonth(9).setDay(9).build();
+        e.setEndD(new_date);
+        assertEquals(new_date, e.getEndD());
     }
 
     @Test
     public void getSetStartT() {
+        assertEquals(default_st, e.getStartT());
+        final TimeOfDay new_time = TimeOfDay.newBuilder().setHours(12).setMinutes(15).setSeconds(0).build();
+        e.setStartT(new_time);
+        assertEquals(new_time, e.getStartT());
     }
 
     @Test
     public void getSetEndT() {
+        assertEquals(default_et, e.getEndT());
+        final TimeOfDay new_time = TimeOfDay.newBuilder().setHours(13).setMinutes(15).setSeconds(0).build();
+        e.setEndT(new_time);
+        assertEquals(new_time, e.getEndT());
     }
 
     @Test
     public void getSetCalendar() {
+        System.out.println("Not yet implemented");
     }
 
 }
