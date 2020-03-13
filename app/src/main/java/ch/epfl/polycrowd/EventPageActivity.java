26312873c12d6d3fc720ebfaa08bacee0378e7e1
yabdro <yassmine.abdrabo@epfl.ch>
2020-03-13 10:22:21 +0100

    retrieves polyevents and displays them, with hard-coded description and image

--- app/src/main/java/ch/epfl/polycrowd/EventPageActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/EventPageActivity.java
@@ -1,5 +1,8 @@
 package ch.epfl.polycrowd;
 
+import android.content.Context;
+import android.util.Log;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,12 +14,22 @@ import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class EventPageActivity extends AppCompatActivity {
 
+
+
     RecyclerView mRecyclerView ;
     MyAdapter myAdapter ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,17 +38,44 @@ public class EventPageActivity extends AppCompatActivity {
         mRecyclerView = findViewById(R.id.recyclerView);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        myAdapter = new MyAdapter(this, getMyList());
+        //To use inside
+        Context context = this ;
+        FirebaseInterface firebaseInterface = new FirebaseInterface();
+        final FirebaseFirestore firestore = firebaseInterface.getFirestoreInstance(false) ;
+        firestore.collection("polyevents").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
-        mRecyclerView.setAdapter(myAdapter);
+                List<FirebaseEventAdaptor> events = queryDocumentSnapshots.toObjects(FirebaseEventAdaptor.class) ;
+                myAdapter = new MyAdapter(context, getModels(events));
+                mRecyclerView.setAdapter(myAdapter);
+
+                Log.v("\nEventPageActivity\n", "\nVALUE OF EVENT1 NAME IS  " + events.get(0).getName() +" \n") ;
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        }) ;
 
     }
 
-    private ArrayList<Model> getMyList(){
+    private ArrayList<Model> getModels(List<FirebaseEventAdaptor> events){
 
         ArrayList<Model> models = new ArrayList<>() ;
 
-        Model m1 = new Model() ;
+        for(FirebaseEventAdaptor e: events)
+        {
+            Model m = new Model() ;
+            m.setTitle(e.getEvent().getName());
+            m.setDescription("Upcoming events");
+            m.setImg(R.drawable.p1);
+            models.add(m) ;
+        }
+
+        /*Model m1 = new Model() ;
         m1.setTitle("Bar");
         m1.setDescription("Upcoming events");
         m1.setImg(R.drawable.p1) ;
@@ -69,7 +109,7 @@ public class EventPageActivity extends AppCompatActivity {
         m6.setTitle("Event6");
         m6.setDescription("Upcoming events");
         m6.setImg(R.drawable.p1) ;
-        models.add(m6) ;
+        models.add(m6) ; */
 
         return models ;
     }
