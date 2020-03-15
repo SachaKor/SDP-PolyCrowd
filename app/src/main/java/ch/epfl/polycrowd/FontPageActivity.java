21f821800ef1396e5ba922db6b83c6d937f77cfc
salimbeni1 <etienne.salimbeni@epfl.ch>
2020-03-15 13:37:50 +0100

    scroll in front-page

---
+++ app/src/main/java/ch/epfl/polycrowd/FontPageActivity.java
@@ -0,0 +1,14 @@
+package ch.epfl.polycrowd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FontPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_font_page);
+    }
+}
