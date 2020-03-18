23a5cdd196e1850a9e1ee1e6d16d9c247d850c70
batkem <demahomirdahk@gmail.com>
2020-03-18 23:14:01 +0100

    added schedule unit test & file permissions

--- app/src/test/java/ch/epfl/polycrowd/ScheduleTest.java
+++ app/src/test/java/ch/epfl/polycrowd/ScheduleTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.nio.file.Paths;
 import java.text.ParseException;
 
@@ -17,16 +18,16 @@ public class ScheduleTest {
 
     @Test
     public void testDownload(){
-        Schedule s = new Schedule();
-        System.out.println(s.downloadIcsFile("https://satellite.bar/agenda/ical.php", true));
+        Schedule s = new Schedule("https://satellite.bar/agenda/ical.php",new File("calendars"));
+        System.out.println(s.downloadIcsFile("https://satellite.bar/agenda/ical.php", new File("calendars")));
 
     }
     @Test
     public void testGetAndParse(){
-        Schedule s = new Schedule();
-        String p = s.downloadIcsFile("https://satellite.bar/agenda/ical.php", true);
+        Schedule s = new Schedule("https://satellite.bar/agenda/ical.php",new File("calendars"));
+        String p = s.downloadIcsFile("https://satellite.bar/agenda/ical.php", new File("calendars"));
         try {
-            s.loadIcs(p);
+            s.loadIcs();
         } catch (ParseException e) {
             e.printStackTrace();
         }
