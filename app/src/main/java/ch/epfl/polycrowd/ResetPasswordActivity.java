493f175228e74c598734ff7d1e7b7b6aaefb74d7
yabdro <yassmine.abdrabo@epfl.ch>
2020-03-23 20:33:06 +0100

    partial implementation of onClicked method for reset button

--- app/src/main/java/ch/epfl/polycrowd/ResetPasswordActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/ResetPasswordActivity.java
@@ -1,8 +1,24 @@
 package ch.epfl.polycrowd;
 
+
+import ch.epfl.polycrowd.firebase.FirebaseInterface;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import ch.epfl.polycrowd.firebase.FirebaseInterface;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
@@ -10,5 +26,27 @@ public class ResetPasswordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+    }
+
+    public void sendResetLinkClicked(View view) {
+
+        EditText email = findViewById(R.id.reset_email) ;
+
+        FirebaseInterface fbi = new FirebaseInterface() ;
+        FirebaseAuth auth = fbi.getAuthInstance(false);
+        auth.sendPasswordResetEmail(email.getText().toString()).addOnCompleteListener(
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(ResetPasswordActivity.this, "A reset link has been sent to your email", Toast.LENGTH_SHORT).show();
+                        } else{
+                            //TODO check if the user exists or not, and if not, suggest signup
+                            //Otherwise, network error?
+                        }
+                    }
+                }
+        ) ;
+
     }
 }
