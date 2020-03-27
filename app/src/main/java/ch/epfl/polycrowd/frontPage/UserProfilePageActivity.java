05f1c37a0e573e3a081053ce237d4dfa7f65bdbc
yabdro <yassmine.abdrabo@epfl.ch>
2020-03-27 22:12:39 +0100

    empty activity for user profile page activity

---
+++ app/src/main/java/ch/epfl/polycrowd/frontPage/UserProfilePageActivity.java
@@ -0,0 +1,16 @@
+package ch.epfl.polycrowd.frontPage;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ch.epfl.polycrowd.R;
+
+public class UserProfilePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile_page);
+    }
+}
