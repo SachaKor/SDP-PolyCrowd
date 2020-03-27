5362d279fdd8c4f2776f4be5b0ea033f4761d416
Cedric Hoelzl <cedric.hoelzl@epfl.ch>
2020-03-27 11:02:42 +0100

    More Tests for EventClass

--- app/src/test/java/ch/epfl/polycrowd/EventTest.java
+++ app/src/test/java/ch/epfl/polycrowd/EventTest.java
@@ -7,15 +7,20 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 import static ch.epfl.polycrowd.Event.dateToString;
 import static ch.epfl.polycrowd.Event.stringToDate;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 
 //import static org.junit.Assert.assertThrows;
 //import static org.junit.jupiter.api.Assertions.*;
@@ -57,11 +62,71 @@ public class EventTest {
                 default_type, default_s, null, "None", default_desc));
         Assert.assertThrows(IllegalArgumentException.class, () -> new Event(default_owner,default_name, false,
                 default_type, default_s, default_e,null, default_desc));
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Event(default_owner,default_name, false,
+                default_type, default_s, default_e,"None", default_desc, (List)null));
+        Event ne = new Event(default_owner,default_name, false,
+                default_type, default_s, default_e,"None", default_desc);
+
+        assert(default_owner.equals(ne.getOwner()));
+        assertEquals(default_name, ne.getName());
+        assertEquals(false, ne.getPublic());
+        assertEquals(default_type, ne.getType());
+        assertEquals(dateToString(default_s, dtFormat), dateToString(ne.getStart(), dtFormat));
+        assertEquals(dateToString(default_e, dtFormat), dateToString(ne.getEnd(), dtFormat));
+        assertEquals("None", ne.getCalendar());
+
+        Event neb = new Event(default_owner,default_name, false,
+                default_type, default_s, default_e,"https://satellite.bar/agenda/ical.php", default_desc, new File("./"));
+
+        assert(default_owner.equals(neb.getOwner()));
+        assertEquals(default_name, neb.getName());
+        assertEquals(false, neb.getPublic());
+        assertEquals(default_type, neb.getType());
+        assertEquals(dateToString(default_s, dtFormat), dateToString(neb.getStart(), dtFormat));
+        assertEquals(dateToString(default_e, dtFormat), dateToString(neb.getEnd(), dtFormat));
+        assertEquals("https://satellite.bar/agenda/ical.php", neb.getCalendar());
+
+        Event nec = new Event(neb, new File("./"));
+
+        assert(default_owner.equals(nec.getOwner()));
+        assertEquals(default_name, nec.getName());
+        assertEquals(false, nec.getPublic());
+        assertEquals(default_type, nec.getType());
+        assertEquals(dateToString(default_s, dtFormat), dateToString(nec.getStart(), dtFormat));
+        assertEquals(dateToString(default_e, dtFormat), dateToString(nec.getEnd(), dtFormat));
+        assertEquals("https://satellite.bar/agenda/ical.php", nec.getCalendar());
+
+        Event ned = new Event();
+        assertNull(ned.getOwner());
+        assertEquals("DEBUG EVENT", ned.getName());
+        assertEquals(true, ned.getPublic());
+        assertEquals(Event.EventType.OTHER, ned.getType());
+        assertNull(ned.getStart());
+        assertNull(ned.getEnd());
+        assertNull(ned.getCalendar());
+        assertEquals("this is only a debug event ... ",ned.getDescription());
+    }
+
+    @Test
+    public void getSetId() {
+        assertThrows(IllegalArgumentException.class, ()-> e.setId(null));
+        e.setId("CustomID");
+        assertEquals("CustomID",e.getId());
     }
 
     @Test
     public void getOwner() {
         assert(default_owner.equals(e.getOwner()));
+
+    }
+
+    @Test
+    public void fakeConstructor(){
+        try {
+            Event.fakeEvent("url", new File("./"));
+        } catch (ParseException ex) {
+            assert(false);
+        }
 
     }
 
@@ -75,11 +140,36 @@ public class EventTest {
     }
 
     @Test
+    public void getSetDescription() {
+        final String new_desc = "NewEventDescription";
+        assertEquals(default_desc, e.getDescription());
+        e.setDescription(new_desc);
+        assertEquals(new_desc, e.getDescription());
+        e.setDescription(null);
+        assertEquals("" , e.getDescription());
+    }
+
+    @Test
     public void getSetPublic() {
         final Boolean new_public = true;
         assertEquals(false, e.getPublic());
         e.setPublic(new_public);
         assertEquals(new_public, e.getPublic());
+    }
+
+    @Test
+    public void getSetOrganizer() {
+        final String new_org = "1";
+        e.addOrganizer(new_org);
+        assert(e.getOrganizers().contains(new_org));
+        e.addOrganizer(null);
+    }
+
+    @Test
+    public void getSetImage() {
+        final int new_image = R.drawable.balelec;
+        e.setImage(new_image);
+        assertEquals(new_image, e.getImage());
     }
 
     @Test
