5df14da30d232f55c04062f6a6185080707bb790
Cedric Hoelzl <cedric.hoelzl@epfl.ch>
2020-03-27 10:00:47 +0100

    Cleaned up User classes

--- app/src/main/java/ch/epfl/polycrowd/SignUpActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/SignUpActivity.java
@@ -1,31 +1,15 @@
 package ch.epfl.polycrowd;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.app.AppCompatActivity;
 import ch.epfl.polycrowd.firebase.FirebaseInterface;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * TODO: refactor if possible
@@ -38,7 +22,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     private final FirebaseInterface fbi = new FirebaseInterface(this);
 
-    @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+    @VisibleForTesting
     public void setMocking(){
         this.fbi.setMocking();
     }
