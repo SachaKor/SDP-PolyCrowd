c9fb236a41ac064a34ab39996d7d9eb255423e09
Gianni Lodetti <gianni.lodetti@epfl.ch>
2020-03-27 10:48:08 +0100

    Modified MapActivity so it now prints the current location of the user as text on the screen TODO: make it update the dot on the map instead

--- app/src/main/java/ch/epfl/polycrowd/map/MapActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/map/MapActivity.java
@@ -1,14 +1,24 @@
 package ch.epfl.polycrowd.map;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.content.Intent;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.SupportMapFragment;
 
@@ -29,6 +39,7 @@ public class MapActivity extends AppCompatActivity {
         VISITOR,
         ORGANISER
     }
+
     // status of the current Activity
     public level status;
 
@@ -46,71 +57,155 @@ public class MapActivity extends AppCompatActivity {
     // eventId is needed to open the correct EventDetails page
     private String eventId;
 
+    //create buttons for testing location
+    private Button locButton;
+    private TextView locView;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
 
-
-    void setGuestButtons( Button buttonLeft , Button  buttonRight){
+    void setGuestButtons(Button buttonLeft, Button buttonRight) {
         buttonRight.setText("EVENT DETAILS");
         buttonLeft.setText("LOGIN");
 
         buttonRight.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { clickEventDetails(v); }
+            public void onClick(View v) {
+                clickEventDetails(v);
+            }
         });
         buttonLeft.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { clickSignIn(v); }
+            public void onClick(View v) {
+                clickSignIn(v);
+            }
         });
     }
 
 
-    void setVisitorButtons( Button buttonLeft , Button  buttonRight){
+    void setVisitorButtons(Button buttonLeft, Button buttonRight) {
         buttonRight.setText("EVENT DETAILS");
         buttonLeft.setText("GROUPS");
 
         buttonRight.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { clickEventDetails(v); }
+            public void onClick(View v) {
+                clickEventDetails(v);
+            }
         });
         buttonLeft.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { }
+            public void onClick(View v) {
+            }
         });
     }
 
-    void setOrganiserButtons(Button buttonLeft , Button  buttonRight){
+    void setOrganiserButtons(Button buttonLeft, Button buttonRight) {
         buttonRight.setText("MANAGE DETAILS");
         buttonLeft.setText("STAFF");
 
         buttonRight.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { clickEventDetails(v); }
+            public void onClick(View v) {
+                clickEventDetails(v);
+            }
         });
         buttonLeft.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { }
+            public void onClick(View v) {
+            }
         });
     }
 
 
-
-    void createButtons(){
+    void createButtons() {
         Button buttonRight = findViewById(R.id.butRight);
         Button buttonLeft = findViewById(R.id.butLeft);
 
-        switch(status)  {
+        switch (status) {
             case GUEST:
-                setGuestButtons(buttonLeft , buttonRight);
+                setGuestButtons(buttonLeft, buttonRight);
                 break;
             case VISITOR:
-                setVisitorButtons(buttonLeft , buttonRight);
+                setVisitorButtons(buttonLeft, buttonRight);
                 break;
             case ORGANISER:
-                setOrganiserButtons(buttonLeft , buttonRight);
+                setOrganiserButtons(buttonLeft, buttonRight);
                 break;
 
         }
+
+        //setup classes for instances needed for getting location
+        locButton = (Button) findViewById(R.id.buttonLocation);
+        locView = (TextView) findViewById(R.id.locView);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                locView.append("\n" + location.getLatitude() + " " + location.getLongitude());
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET
+            }, 10);
+            return;
+        } else {
+            configureButton();
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case 10:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                        configureButton();
+            return;
+        }
+    }
+
+    private void configureButton() {
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        locButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View view) {
+                //noinspection MissingPermission
+                locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+            }
+        });
+    }
+
 
     void createMap(){
         // display map  WARNING: TO DO AT THE END OF ONCREATE
