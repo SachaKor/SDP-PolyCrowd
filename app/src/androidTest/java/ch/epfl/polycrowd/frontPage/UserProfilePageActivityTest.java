15cffd287acd583eebea39b49a88ab2ef5b273a0
yabdro <hafez435@gmail.com>
2020-04-03 03:05:52 +0200

    add userProfile sub-package

---
+++ app/src/androidTest/java/ch/epfl/polycrowd/frontPage/UserProfilePageActivityTest.java
@@ -0,0 +1,41 @@
+package ch.epfl.polycrowd.frontPage;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import ch.epfl.polycrowd.R;
+import ch.epfl.polycrowd.frontPage.userProfile.UserEventListActivity;
+import ch.epfl.polycrowd.logic.PolyContext;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertTrue;
+
+
+public class UserProfilePageActivityTest {
+
+
+    @Test
+    public void checkTestMockingEnabled(){
+        assertTrue(PolyContext.isRunningTest());
+    }
+
+    @Rule
+    public final ActivityTestRule<UserEventListActivity.UserProfilePageActivity> userProfileActivityRule =
+            new ActivityTestRule<>(UserEventListActivity.UserProfilePageActivity.class);
+    @Test
+    public void usernameFieldShowsCurrentUserValue() {
+
+        onView(withId(R.id.usernameField)).check(matches(withText("fake user"))) ;
+    }
+
+    @Test
+    public void emailFieldShowsCurrentUserValue() {
+
+        onView(withId(R.id.emailField)).check(matches(withText("fake@fake.com"))) ;
+    }
+}
