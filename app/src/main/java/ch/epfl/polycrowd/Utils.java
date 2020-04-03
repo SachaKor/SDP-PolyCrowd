c8e440815730da14adf085bf31aa9ec78fb315d8
yabdro <hafez435@gmail.com>
2020-04-03 02:56:59 +0200

    add static method that returns fixed fake user

--- app/src/main/java/ch/epfl/polycrowd/Utils.java
+++ app/src/main/java/ch/epfl/polycrowd/Utils.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.Gravity;
 import android.widget.Toast;
 
+import ch.epfl.polycrowd.logic.User;
+
 
 public class Utils {
 
@@ -17,6 +19,14 @@ public class Utils {
         Toast toast = Toast.makeText(context, text, duration);
         toast.setGravity(Gravity.BOTTOM, 0, 16);
         toast.show();
+    }
+
+    public static User getFakeUser(){
+        return new User("fake@fake.com", "1", "fake user", 100);
+    }
+
+    public Event getFakeEvent(){
+        return new Event() ;
     }
 
 
