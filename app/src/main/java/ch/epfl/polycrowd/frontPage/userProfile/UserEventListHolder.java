f407f83bdfc88130acebe9fce137470075d50c1c
yabdro <hafez435@gmail.com>
2020-04-03 03:00:03 +0200

    RecyclerView holder for user's events list

---
+++ app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserEventListHolder.java
@@ -0,0 +1,37 @@
+package ch.epfl.polycrowd.frontPage.userProfile;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ch.epfl.polycrowd.ItemClickListener;
+import ch.epfl.polycrowd.R;
+
+public class UserEventListHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    ImageView mImaeView ;
+    TextView mTitle, mDes ;
+    ItemClickListener itemClickListener ;
+
+    public UserEventListHolder(@NonNull View itemView) {
+        super(itemView);
+
+        mImaeView = itemView.findViewById(R.id.imageIv) ;
+        mTitle = itemView.findViewById(R.id.titleTv) ;
+        mDes = itemView.findViewById(R.id.descriptionTv) ;
+
+        itemView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onItemClickListener(v, getLayoutPosition());
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+}
