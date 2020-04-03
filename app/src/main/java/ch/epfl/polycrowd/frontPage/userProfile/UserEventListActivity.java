b691b2ba9e7e55ea893a493b54cd695b0b0a47d0
yabdro <hafez435@gmail.com>
2020-04-03 23:59:37 +0200

    getModelList filters events where the user is organiser

--- app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserEventListActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserEventListActivity.java
@@ -2,7 +2,6 @@ package ch.epfl.polycrowd.frontPage.userProfile;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,21 +9,23 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import ch.epfl.polycrowd.Event;
 import ch.epfl.polycrowd.R;
 import ch.epfl.polycrowd.firebase.FirebaseInterface;
 import ch.epfl.polycrowd.logic.Model;
+import ch.epfl.polycrowd.logic.User;
 
 public class UserEventListActivity  extends AppCompatActivity {
 
+
+    private static final String TAG = "UserEventListActivity" ;
 
     private RecyclerView mRecyclerView;
     private UserEventListAdapter mAdapter ;
 
     private FirebaseInterface fi = new FirebaseInterface(this) ;
-
-    private int calls = 0 ;
 
     //TODO: Create an extra field for the events for which the user is an organiser
     //For now, use a hard-coded event-list
@@ -46,16 +47,23 @@ public class UserEventListActivity  extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.O)
     private ArrayList<Model> getModelList(){
         ArrayList<Model> models = new ArrayList<>() ;
+        User currUser = fi.getCurrentUser();
 
         //Use Firebase to get the events:
         fi.getAllEvents(events -> {
-            Event.toModels(events).forEach(e -> {models.add(e) ; ++calls;});
+
+            List<Event> filtered = new ArrayList<>() ;
+            events.forEach( e -> {
+                if(e.getOrganizers().contains(currUser.getEmail())){
+                    filtered.add(e) ;
+                } ;
+            });
+            //Filtered events to models
+            Event.toModels(filtered).forEach(e -> models.add(e));
             mAdapter.notifyDataSetChanged();
-            Log.d("UserEventListActivity", "\n Count is "+calls+"\n") ;
         });
 
         return models ;
-
     }
 
 }
