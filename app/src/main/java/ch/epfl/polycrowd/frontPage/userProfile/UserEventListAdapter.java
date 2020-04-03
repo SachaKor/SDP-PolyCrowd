54449f16b9eba5f2d12201447f1cade38dc93713
yabdro <hafez435@gmail.com>
2020-04-03 21:26:10 +0200

    use 3-argument inflate method

--- app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserEventListAdapter.java
+++ app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserEventListAdapter.java
@@ -34,7 +34,7 @@ public class UserEventListAdapter extends RecyclerView.Adapter<UserEventListHold
     @NonNull
     @Override
     public UserEventListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, null) ;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false) ;
 
         return new UserEventListHolder(view);
     }
