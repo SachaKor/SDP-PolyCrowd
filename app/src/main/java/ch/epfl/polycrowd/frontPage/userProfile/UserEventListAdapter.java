633ddbd339fbfb64f45604e6c0be96d9b47e04a2
yabdro <hafez435@gmail.com>
2020-04-03 11:38:49 +0200

    todo comments in call to holder setItemClickListener

--- app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserEventListAdapter.java
+++ app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserEventListAdapter.java
@@ -58,6 +58,9 @@ public class UserEventListAdapter extends RecyclerView.Adapter {
             String eventId = "ADYz6HuISjOiG4uBRY2z"; //id is the string that identifies the specific document?
             //event.setId(eventId);
             //PolyContext.setCurrentEvent(event);
+            //TODO figure out how to use position argument in the lambda to retrieve the eventId
+            //TODO: Use position argument to get the model in the list, and then from the model,getId
+            //TODO Question, where is the model id set?
 
             FirebaseInterface fi = new FirebaseInterface(c);
             try {
@@ -78,4 +81,5 @@ public class UserEventListAdapter extends RecyclerView.Adapter {
     public int getItemCount() {
         return models.size();
     }
+
 }
