7eb1e222681cd0124e9ae358352e8f522b4ab282
yabdro <hafez435@gmail.com>
2020-04-05 23:56:10 +0200

    methods that return success and failure handlers, added releveant toastDisplays for each handler

--- app/src/main/java/ch/epfl/polycrowd/LoginActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/LoginActivity.java
@@ -17,6 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.Objects;
 
 import ch.epfl.polycrowd.firebase.FirebaseInterface;
+import ch.epfl.polycrowd.firebase.handlers.UserHandler;
 import ch.epfl.polycrowd.logic.Event;
 import ch.epfl.polycrowd.logic.PolyContext;
 
@@ -57,44 +58,64 @@ public class LoginActivity extends AppCompatActivity {
                 passwordEditText = findViewById(R.id.sign_in_pswd);
         String email = emailEditText.getText().toString(),
                 password = passwordEditText.getText().toString();
+
         if(email.isEmpty()) {
             toastPopup("Enter your email");
             return;
         }
+
         if(password.isEmpty()) {
             toastPopup("Enter your password");
             return;
         }
 
-        Context c = this;
-        fbInterface.signInWithEmailAndPassword(email, password, user -> {
-            if(fbInterface.getCurrentUser() == null) {
-                return;
-            }
-            PolyContext.setCurrentUser(user);
-            /* if the user logs in to accept the organizer invitation, add him/her to the
-                organizers list, then open the event details page for the preview */
-            Log.d(TAG, "previous page: " + PolyContext.getPreviousPage());
-            if(PolyContext.getPreviousPage().equals("OrganizerInviteActivity")) {
-                String organizerEmail =
-                        Objects.requireNonNull(fbInterface.getCurrentUser().getEmail());
-                if(PolyContext.getCurrentEvent() == null) {
-                    Log.e(TAG, "current event is null");
-                    return;
-                }
-                Event event = PolyContext.getCurrentEvent();
-                fbInterface.addOrganizerToEvent(event.getId(), organizerEmail, () -> {
-                    Intent eventDetails = new Intent(c, EventPageDetailsActivity.class);
-                    eventDetails.putExtra(EVENT_ID, event.getId());
-                    startActivity(eventDetails);
-                });
-            }
-        });
+        fbInterface.signInWithEmailAndPassword(email, password, successHandler(), failureHandler());
     }
 
 
     public void forgotPasswordButtonClicked(View view){
         Intent intent = new Intent(this, ResetPasswordActivity.class) ;
         startActivity(intent);
+    }
+
+    private UserHandler failureHandler(){
+        Context c = this ;
+        return user ->Toast.makeText(c, "Incorrect email or password", Toast.LENGTH_SHORT).show();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private UserHandler successHandler(){
+        return user->{
+                //When would the currentUser be null if signin was successful?
+                Context c = this;
+                if(fbInterface.getCurrentUser() == null) {
+                    return;
+                }
+
+            Toast.makeText(c, "Sign in success", Toast.LENGTH_SHORT).show();
+            PolyContext.setCurrentUser(user);
+
+
+            /* if the user logs in to accept the organizer invitation, add him/her to the
+                organizers list, then open the event details page for the preview */
+                Log.d(TAG, "previous page: " + PolyContext.getPreviousPage());
+                if(PolyContext.getPreviousPage().equals("OrganizerInviteActivity")) {
+                    String organizerEmail =
+                            Objects.requireNonNull(fbInterface.getCurrentUser().getEmail());
+                    if(PolyContext.getCurrentEvent() == null) {
+                        Log.e(TAG, "current event is null");
+                        return;
+                    }
+                    Event event = PolyContext.getCurrentEvent();
+                    fbInterface.addOrganizerToEvent(event.getId(), organizerEmail, () -> {
+                        Intent eventDetails = new Intent(c, EventPageDetailsActivity.class);
+                        eventDetails.putExtra(EVENT_ID, event.getId());
+                        startActivity(eventDetails);
+                    });
+                }
+
+
+        } ;
     }
 }
