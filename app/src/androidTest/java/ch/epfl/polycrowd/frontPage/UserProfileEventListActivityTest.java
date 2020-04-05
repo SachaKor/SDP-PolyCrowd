b16dd8620c8433d6ae240ff588b4e9904a9042e5
yabdro <hafez435@gmail.com>
2020-04-05 17:55:16 +0200

    tests that didn't work using test recorder

---
+++ app/src/androidTest/java/ch/epfl/polycrowd/frontPage/UserProfileEventListActivityTest.java
@@ -0,0 +1,360 @@
+package ch.epfl.polycrowd.frontPage;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.polycrowd.R;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class UserProfileEventListActivityTest {
+
+    @Rule
+    public ActivityTestRule<FrontPageActivity> mActivityTestRule = new ActivityTestRule<>(FrontPageActivity.class);
+
+    @Test
+    public void userProfileEventListActivityTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.button), withText("LOGIN"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.sign_in_email),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.sign_in_scroll),
+                                        0),
+                                1)));
+        appCompatEditText.perform(scrollTo(), replaceText("nani@haha.com"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.sign_in_email), withText("nani@haha.com"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.sign_in_scroll),
+                                        0),
+                                1)));
+        appCompatEditText2.perform(scrollTo(), click());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.sign_in_pswd),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.sign_in_scroll),
+                                        0),
+                                2)));
+        appCompatEditText3.perform(scrollTo(), replaceText("123456"), closeSoftKeyboard());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.sign_in_button), withText("Sign in"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.sign_in_scroll),
+                                        0),
+                                3)));
+        appCompatButton2.perform(scrollTo(), click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.goToUserProfileButton), withText("Profile"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.goToUserEventsListButton), withText("Events"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatButton4.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction cardView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.userEventListRecyclerView),
+                                childAtPosition(
+                                        withId(R.id.userEventListLayout),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        cardView.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction relativeLayout = onView(
+                allOf(withId(R.id.event_item),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.userEventListRecyclerView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        relativeLayout.check(matches(isDisplayed()));
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.userEventListRecyclerView),
+                        childAtPosition(
+                                allOf(withId(R.id.userEventListLayout),
+                                        childAtPosition(
+                                                withId(android.R.id.content),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        recyclerView.check(matches(isDisplayed()));
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.titleTv), withText("Date test -.-"),
+                        childAtPosition(
+                                allOf(withId(R.id.event_item),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("Date test -.-")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.descriptionTv), withText("default description"),
+                        childAtPosition(
+                                allOf(withId(R.id.event_item),
+                                        childAtPosition(
+                                                IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        textView2.check(matches(withText("default description")));
+
+        ViewInteraction relativeLayout2 = onView(
+                allOf(withId(R.id.event_item),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.userEventListRecyclerView),
+                                        0),
+                                0),
+                        isDisplayed()));
+        relativeLayout2.check(matches(isDisplayed()));
+
+        ViewInteraction cardView2 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.userEventListRecyclerView),
+                                childAtPosition(
+                                        withId(R.id.userEventListLayout),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        cardView2.perform(click());
+
+        ViewInteraction cardView3 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.userEventListRecyclerView),
+                                childAtPosition(
+                                        withId(R.id.userEventListLayout),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        cardView3.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        pressBack();
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction cardView4 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.userEventListRecyclerView),
+                                childAtPosition(
+                                        withId(R.id.userEventListLayout),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        cardView4.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.event_details_title), withText("Date test -.-"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        textView3.check(matches(withText("Date test -.-")));
+
+        ViewInteraction relativeLayout3 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(android.R.id.content),
+                                childAtPosition(
+                                        withId(R.id.action_bar_root),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        relativeLayout3.check(matches(isDisplayed()));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
