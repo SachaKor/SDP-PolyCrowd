5e457399edc383658463a2b34d520140098a7fb7
yabdro <hafez435@gmail.com>
2020-04-07 00:01:35 +0200

    handles case of non-registered user using getUserByEmail,defines and passes appropriate handlers for each case

--- app/src/main/java/ch/epfl/polycrowd/ResetPasswordActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/ResetPasswordActivity.java
@@ -1,15 +1,17 @@
 package ch.epfl.polycrowd;
 
 
-import ch.epfl.polycrowd.firebase.FirebaseInterface;
-
-import androidx.annotation.VisibleForTesting;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import ch.epfl.polycrowd.firebase.FirebaseInterface;
+import ch.epfl.polycrowd.firebase.handlers.UserHandler;
 
 
 public class ResetPasswordActivity extends AppCompatActivity {
@@ -25,17 +27,19 @@ public class ResetPasswordActivity extends AppCompatActivity {
         this.fbi = new FirebaseInterface(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void sendResetLinkClicked(View view) {
-
         String email = ((EditText) findViewById(R.id.reset_email)).getText().toString();
         if (email.isEmpty()) {
             Toast.makeText(ResetPasswordActivity.this, "Please enter an email", Toast.LENGTH_SHORT).show();
         } else{
-
             //First need to query the database to exclude that this email is not registered
-            fbi.resetPassword(email);
-
+            UserHandler pwResetSuccess = user -> Utils.toastPopup(this, "A reset link has been sent to your email") ;
+            UserHandler pwResetFailure = user -> Utils.toastPopup(this, "Connection error, please try again later") ;
+            UserHandler emailFoundHandler = user -> fbi.resetPassword(email, pwResetSuccess, pwResetFailure);
+            UserHandler emailNotFoundHandler = u -> Utils.toastPopup(this, "Email not found, please sign up") ;
+            //Finally put everything together
+            fbi.getUserByEmail(email, emailFoundHandler, emailNotFoundHandler);
         }
-
     }
 }
