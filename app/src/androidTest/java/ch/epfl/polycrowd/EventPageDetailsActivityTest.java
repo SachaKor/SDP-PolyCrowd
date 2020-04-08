a0a8d90b34f8c25ea8d5d7932b90b0d62a149383
SashaKor <alexandra.korukova@gmail.com>
2020-04-08 17:25:34 +0200

    EventDetails page test refactored

--- app/src/androidTest/java/ch/epfl/polycrowd/EventPageDetailsActivityTest.java
+++ app/src/androidTest/java/ch/epfl/polycrowd/EventPageDetailsActivityTest.java
@@ -7,11 +7,21 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import ch.epfl.polycrowd.firebase.DatabaseInterface;
+import ch.epfl.polycrowd.firebase.FirebaseMocker;
+import ch.epfl.polycrowd.firebase.handlers.UserHandler;
 import ch.epfl.polycrowd.logic.Event;
 import ch.epfl.polycrowd.logic.PolyContext;
+import ch.epfl.polycrowd.logic.User;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -26,10 +36,8 @@ public class EventPageDetailsActivityTest {
 
     private static final String TAG = "EventPageDetailsTest";
 
-    @Test
-    public void checkTestMockingEnabled(){
-        assertTrue(PolyContext.isRunningTest());
-    }
+    private List<Event> events;
+    private Map<User, String> usersAndPasswords;
 
     @Rule
     // https://stackoverflow.com/questions/31388847/how-to-get-the-activity-reference-before-its-oncreate-gets-called-during-testing
@@ -39,7 +47,20 @@ public class EventPageDetailsActivityTest {
 
     @Before
     public void startIntent() {
-        Event ev = new Event();
+        Date sDate = new Date(1649430344),
+                eDate = new Date(1649516744);
+        Event ev = new Event("eventOwner", "testEvent", true, Event.EventType.CONCERT,
+                sDate, eDate, "testCalendar", "testDescription");
+        ev.setId("1");
+        User user = new User("organizer@op.com", "1", "organizer", 3L);
+        ev.addOrganizer(user.getEmail());
+        usersAndPasswords = new HashMap<>();
+        events = new ArrayList<>();
+        events.add(ev);
+        usersAndPasswords.put(user, "123456");
+        DatabaseInterface dbi = new FirebaseMocker(usersAndPasswords, events);
+        PolyContext.setDbInterface(dbi);
+        PolyContext.setCurrentUser(user);
         PolyContext.setCurrentEvent(ev);
         Intent intent = new Intent();
         mActivityRule.launchActivity(intent);
