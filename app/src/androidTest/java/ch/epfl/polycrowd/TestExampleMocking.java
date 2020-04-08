86383f1aee09b2b8e6878936f53e65ea4a23a466
SashaKor <alexandra.korukova@gmail.com>
2020-04-08 17:25:34 +0200

    EventDetails page test refactored

--- app/src/androidTest/java/ch/epfl/polycrowd/TestExampleMocking.java
+++ app/src/androidTest/java/ch/epfl/polycrowd/TestExampleMocking.java
@@ -69,8 +69,8 @@ public class TestExampleMocking {
     @Test
     public void showsSignInSuccessToast(){
         onView(withId(R.id.sign_in_email)).perform(typeText(users.get(0).getEmail()), closeSoftKeyboard());
-     onView(withId(R.id.sign_in_pswd)).perform(typeText(usersAndPasswords.get(users.get(0))), closeSoftKeyboard());
-  onView(withId(R.id.sign_in_button)).perform(click());
+        onView(withId(R.id.sign_in_pswd)).perform(typeText(usersAndPasswords.get(users.get(0))), closeSoftKeyboard());
+        onView(withId(R.id.sign_in_button)).perform(click());
         onView(withText("Sign in success"))
                 .inRoot(withDecorView(not(mActivityRule.getActivity().getWindow().getDecorView())))
                 .check(matches(isDisplayed()));
