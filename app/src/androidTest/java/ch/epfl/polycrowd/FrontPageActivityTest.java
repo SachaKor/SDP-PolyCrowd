69759ae729e02a2cf71d1fba03b606fa6d9ddc46
SashaKor <alexandra.korukova@gmail.com>
2020-04-08 17:59:45 +0200

    FrontPageTest refactored

--- app/src/androidTest/java/ch/epfl/polycrowd/FrontPageActivityTest.java
+++ app/src/androidTest/java/ch/epfl/polycrowd/FrontPageActivityTest.java
@@ -1,14 +1,29 @@
 package ch.epfl.polycrowd;
 
+import android.Manifest;
+import android.content.Intent;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import androidx.test.rule.GrantPermissionRule;
+import ch.epfl.polycrowd.firebase.DatabaseInterface;
+import ch.epfl.polycrowd.firebase.FirebaseMocker;
 import ch.epfl.polycrowd.frontPage.FrontPageActivity;
+import ch.epfl.polycrowd.logic.Event;
 import ch.epfl.polycrowd.logic.PolyContext;
+import ch.epfl.polycrowd.logic.User;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -21,16 +36,42 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class FrontPageActivityTest {
 
-    @Test
-    public void checkTestMockingEnabled(){
-        assertTrue(PolyContext.isRunningTest());
-    }
-
     @Rule
     public final ActivityTestRule<FrontPageActivity> frontPageActivityRule =
-            new ActivityTestRule<>(FrontPageActivity.class);
+            new ActivityTestRule<>(FrontPageActivity.class, true, false);
 
+    @Rule
+    public GrantPermissionRule grantCoarseLocation =
+            GrantPermissionRule.grant(Manifest.permission.ACCESS_COARSE_LOCATION);
 
+    @Rule
+    public GrantPermissionRule grantFineLocation =
+            GrantPermissionRule.grant(Manifest.permission.ACCESS_FINE_LOCATION);
+
+    @Before
+    public void setUp() {
+        Date sDate = new Date(1649430344),
+                eDate = new Date(1649516744);
+
+        // events setup
+        Event ev = new Event("eventOwner", "DEBUG EVENT", true, Event.EventType.CONCERT,
+                sDate, eDate, "testCalendar", "this is only a debug event ... ");
+        ev.setId("1");
+        List<Event> events = new ArrayList<>();
+        events.add(ev);
+
+        // users setup
+        Map<User, String> usersAndPasswords = new HashMap<>();
+        usersAndPasswords.put(new User("fake@user", "1", "fakeUser", 3L), "1234567");
+
+        // database interface setup
+        DatabaseInterface dbi = new FirebaseMocker(usersAndPasswords, events);
+        PolyContext.setDbInterface(dbi);
+
+        // launch the intent
+        Intent intent = new Intent();
+        frontPageActivityRule.launchActivity(intent);
+    }
 
     @Test
     public void testDisplaysTitle() {
