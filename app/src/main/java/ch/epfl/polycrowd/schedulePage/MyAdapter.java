859abf74ce19f778bba54f229e4dc4e36f9e55ff
Cedric Hoelzl <cedric.hoelzl@epfl.ch>
2020-04-16 01:18:56 +0200

    Fixed Schedule not Loaded Properly

--- app/src/main/java/ch/epfl/polycrowd/schedulePage/MyAdapter.java
+++ app/src/main/java/ch/epfl/polycrowd/schedulePage/MyAdapter.java
@@ -10,7 +10,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import ch.epfl.polycrowd.R;
