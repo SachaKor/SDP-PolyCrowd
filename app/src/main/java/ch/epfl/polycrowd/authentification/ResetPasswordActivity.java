65401460e530863fe5c1dd6b9abf9782be852e94
Cedric Hoelzl <cedric.hoelzl@epfl.ch>
2020-04-15 23:25:52 +0200

    Cleaned Authentication

--- app/src/main/java/ch/epfl/polycrowd/authentification/ResetPasswordActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/authentification/ResetPasswordActivity.java
@@ -12,21 +12,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import ch.epfl.polycrowd.R;
 import ch.epfl.polycrowd.Utils;
-import ch.epfl.polycrowd.firebase.DatabaseInterface;
 import ch.epfl.polycrowd.firebase.handlers.UserHandler;
 import ch.epfl.polycrowd.logic.PolyContext;
 
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
-
-    private DatabaseInterface dbi;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
-        this.dbi = PolyContext.getDatabaseInterface();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -38,10 +33,10 @@ public class ResetPasswordActivity extends AppCompatActivity {
             //First need to query the database to exclude that this email is not registered
             UserHandler pwResetSuccess = user -> Utils.toastPopup(this, "A reset link has been sent to your email") ;
             UserHandler pwResetFailure = user -> Utils.toastPopup(this, "Connection error, please try again later") ;
-            UserHandler emailFoundHandler = user -> dbi.resetPassword(email, pwResetSuccess, pwResetFailure);
+            UserHandler emailFoundHandler = user -> PolyContext.getDatabaseInterface().resetPassword(email, pwResetSuccess, pwResetFailure);
             UserHandler emailNotFoundHandler = u -> Utils.toastPopup(this, "Email not found, please sign up") ;
             //Finally put everything together
-            dbi.getUserByEmail(email, emailFoundHandler, emailNotFoundHandler);
+            PolyContext.getDatabaseInterface().getUserByEmail(email, emailFoundHandler, emailNotFoundHandler);
         }
     }
 }
