20259bb7555a0801a9d6229fac228bea27321972
Cedric Hoelzl <cedric.hoelzl@epfl.ch>
2020-04-15 23:25:21 +0200

    Cleaned Logic

--- app/src/main/java/ch/epfl/polycrowd/logic/Storable.java
+++ app/src/main/java/ch/epfl/polycrowd/logic/Storable.java
@@ -1,14 +1,9 @@
 package ch.epfl.polycrowd.logic;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public abstract class  Storable {
 
-    /**
-     * returns the relevant data to store in the database
-     * @return
-     */
     public abstract Map<String, Object> getRawData();
 
 }
