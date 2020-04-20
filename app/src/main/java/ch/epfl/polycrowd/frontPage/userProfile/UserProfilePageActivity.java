0e23392ccdd324164055a3a660db6f1c75bd6a11
Gianniii <Gianni.lodetti@hotmail.com>
2020-04-20 14:38:33 +0200

    added edit icons to user profile and refactored userProfileActivity slightly

--- app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserProfilePageActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserProfilePageActivity.java
@@ -2,6 +2,7 @@ package ch.epfl.polycrowd.frontPage.userProfile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -14,25 +15,39 @@ import ch.epfl.polycrowd.logic.User;
 
 public class UserProfilePageActivity extends AppCompatActivity {
 
-    private User user ;
+    private User user;
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
-        View view = LayoutInflater.from(this).inflate(R.layout.activity_user_profile_page, null, false) ;
+        view = LayoutInflater.from(this).inflate(R.layout.activity_user_profile_page, null, false) ;
 
-        user = PolyContext.getCurrentUser() ;
-
-        TextView usernameField = view.findViewById(R.id.profileUserName) ;
-        usernameField.setText(user.getName());
-
-        TextView emailField = view.findViewById(R.id.profileEmail) ;
-        emailField.setText(user.getEmail());
+        User AuthUser = PolyContext.getCurrentUser();
+        //TODO: fix firebase interface so i dont have to do this extra step
+        //quick fix since user in polycontext doesnt have correct username for the user we need to fetch user from database
+        PolyContext.getDatabaseInterface().getUserByEmail(AuthUser.getEmail(), u->Success(u), u->Failure(u));
 
         setContentView(view);
 
+    }
+
+    void Success(User u){
+        user = u;
+        TextView usernameField = view.findViewById(R.id.profileUserName);
+        usernameField.setText(user.getName());
+        TextView emailField = view.findViewById(R.id.profileEmail);
+        emailField.setText(user.getEmail());
+    }
+    void Failure(User u){
+        user = null;
+        TextView usernameField = view.findViewById(R.id.profileUserName);
+        usernameField.setText("default username(failed to fetch username");
+        TextView emailField = view.findViewById(R.id.profileEmail);
+        emailField.setText("default email(failed to fetch email from database");
+        user = null;
     }
 
     public void onEventsListClick(View view) {
@@ -46,4 +61,9 @@ public class UserProfilePageActivity extends AppCompatActivity {
     public void OnUserProfileEditImgClick(View view) {
     }
 
+    public void OnUserProfileEditPasswordButtonClick(View view) {
+    }
+
+    public void OnUserProfileEditEmailButtonClick(View view) {
+    }
 }
