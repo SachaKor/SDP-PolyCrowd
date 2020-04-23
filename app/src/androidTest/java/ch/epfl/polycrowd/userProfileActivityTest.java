deace7b39f7cf5f426ebab85d206bcfeceda1c18
Gianniii <Gianni.lodetti@hotmail.com>
2020-04-23 15:39:17 +0200

    User profile activity now users Polycontext User instead of looking up using directly in firebase

---
+++ app/src/androidTest/java/ch/epfl/polycrowd/userProfileActivityTest.java
@@ -0,0 +1,94 @@
+package ch.epfl.polycrowd;
+
+//import com.google.android.gms.tasks.OnSuccessListener;
+//import com.google.firebase.auth.AuthResult;
+
+//import org.junit.AfterClass;
+//import org.junit.BeforeClass;
+import android.content.Intent;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import androidx.test.rule.ActivityTestRule;
+
+import net.sf.ehcache.search.expression.And;
+
+import java.security.acl.Owner;
+
+import ch.epfl.polycrowd.authentification.LoginActivity;
+import ch.epfl.polycrowd.frontPage.userProfile.UserProfilePageActivity;
+import ch.epfl.polycrowd.logic.PolyContext;
+
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static ch.epfl.polycrowd.AndroidTestHelper.sleep;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.StringContains.containsString;
+
+public class userProfileActivityTest {
+
+    @BeforeClass
+    public static void setUpBeforeActivityLaunch(){
+        PolyContext.reset();
+    }
+
+    @Rule
+    public final ActivityTestRule<UserProfilePageActivity> userProfileActivityRule =
+            new ActivityTestRule<UserProfilePageActivity>(UserProfilePageActivity.class, true, false);
+
+    @Before
+    public void setUp() {
+        AndroidTestHelper.SetupMockDBI();
+
+        PolyContext.setCurrentUser(AndroidTestHelper.getNewUser());
+
+        Intent intent = new Intent();
+        userProfileActivityRule.launchActivity(intent);
+    }
+
+    @Test
+    public void testChangePasswordClickOpensDialog(){
+        onView(withId(R.id.profileEditPasswordButton)).perform(click());
+        sleep();
+        onView(withId(0)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testCorrectlyLoadsDataFromUser(){
+       /* onView(withId(R.id.profileEditPasswordButton)).perform(click());
+        sleep();
+        onView(withId(0)).check(matches(isDisplayed()));*/
+    }
+
+    @Test
+    public void changesPasswordSuccefully(){
+       /* onView(withId(R.id.profileEditPasswordButton)).perform(click());
+        sleep();
+        onView(withId(0)).check(matches(isDisplayed()));*/
+    }
+
+    @Test
+    public void failsToChangePassword(){
+       /* onView(withId(R.id.profileEditPasswordButton)).perform(click());
+        sleep();
+        onView(withId(0)).check(matches(isDisplayed()));*/
+    }
+
+    @Test
+    public void myEventsButtonWorks(){
+       /* onView(withId(R.id.profileEditPasswordButton)).perform(click());
+        sleep();
+        onView(withId(0)).check(matches(isDisplayed()));*/
+    }
+}
+
--- app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserProfilePageActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/frontPage/userProfile/UserProfilePageActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,8 +24,11 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import ch.epfl.polycrowd.R;
+import ch.epfl.polycrowd.firebase.FirebaseInterface;
 import ch.epfl.polycrowd.logic.PolyContext;
 import ch.epfl.polycrowd.logic.User;
 
@@ -34,36 +39,26 @@ public class UserProfilePageActivity extends AppCompatActivity {
     AlertDialog dialog;
     EditText editText;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
 
         view = LayoutInflater.from(this).inflate(R.layout.activity_user_profile_page, null, false) ;
-
-        FirebaseUser AuthUser = FirebaseAuth.getInstance().getCurrentUser();
-        //TODO: fix firebase interface so i dont have to do this extra step
-        //quick fix since user in polycontext doesnt have correct username for the user we need to fetch user from database
-        PolyContext.getDatabaseInterface().getUserByEmail(AuthUser.getEmail(), u->Success(u), u->Failure(u));
-
+        setUpTextFields();
         setContentView(view);
 
     }
 
-    void Success(User u){
-        user = u;
+    void setUpTextFields() {
+        //FirebaseUser AuthUser = FirebaseAuth.getInstance().getCurrentUser();
+        //PolyContext.getDatabaseInterface().getUserByEmail(AuthUser.getEmail(), u->Success(u), u->Failure(u));
+        user = PolyContext.getCurrentUser();
         TextView usernameField = view.findViewById(R.id.profileUserName);
         usernameField.setText(user.getName());
         TextView emailField = view.findViewById(R.id.profileEmail);
         emailField.setText(user.getEmail());
-    }
-    void Failure(User u){
-        user = null;
-        TextView usernameField = view.findViewById(R.id.profileUserName);
-        usernameField.setText("default username(failed to fetch username");
-        TextView emailField = view.findViewById(R.id.profileEmail);
-        emailField.setText("default email(failed to fetch email from database");
-        user = null;
     }
 
     public void onEventsListClick(View view) {
@@ -79,15 +74,28 @@ public class UserProfilePageActivity extends AppCompatActivity {
 
     public void OnUserProfileEditPasswordButtonClick(View view) {
         TextView textView = (TextView) findViewById(R.id.profilePassword);
-        setupDialogForCredentials(textView);
+        setupDialogForPasswordChangeCredentials(textView);
 
     }
+    public void OnUserProfileEditEmailButtonClick(View view) {
+        //TextView textView = (TextView) findViewById(R.id.profileEmail);
+        //setupDialogForEmailChangeCredentials(textView);
+        //TODO add options to change email
+    }
+
+
     // Prompt the user to re-provide their sign-in credentials
-    private void setupDialogForCredentials(TextView textView) {
+    private void setupDialogForPasswordChangeCredentials(TextView textView) {
         dialog = new AlertDialog.Builder(this).create();
         EditText emailText = new EditText(this);
+        emailText.setId(0);
+        emailText.setHint("Enter email");
         EditText oldPasswordText= new EditText(this);
+        oldPasswordText.setHint("enter current password");
+        oldPasswordText.setTransformationMethod(PasswordTransformationMethod.getInstance());
         EditText newPasswordText = new EditText(this);
+        newPasswordText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        newPasswordText.setHint("enter new password");
         editText = new EditText(this);
         AuthCredential credential =  EmailAuthProvider
                 .getCredential("doesnt matter", "wont work by default");
@@ -95,11 +103,8 @@ public class UserProfilePageActivity extends AppCompatActivity {
         Context context = textView.getContext();
         LinearLayout layout = new LinearLayout(context);
         layout.setOrientation(LinearLayout.VERTICAL);
-        emailText.setHint("Enter email");
         layout.addView(emailText);
-        oldPasswordText.setHint("enter current password");
         layout.addView(oldPasswordText);
-        newPasswordText.setHint("enter new password");
         layout.addView(newPasswordText);
         dialog.setView(layout); // Again this is a set method, not add
         dialog.setTitle("Change password");
@@ -116,36 +121,99 @@ public class UserProfilePageActivity extends AppCompatActivity {
         dialog.show();
     }
 
-    public void OnUserProfileEditEmailButtonClick(View view) {
-        TextView textView = (TextView) findViewById(R.id.profileEmail);
-        setupDialogForCredentials(textView);
+    private void setupDialogForEmailChangeCredentials(TextView textView) {
+        dialog = new AlertDialog.Builder(this).create();
+        EditText emailText = new EditText(this);
+        emailText.setHint("Enter email");
+        EditText passwordText= new EditText(this);
+        passwordText.setHint("enter current password");
+        passwordText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        EditText newEmailText = new EditText(this);
+        newEmailText.setHint("enter new email");
+        editText = new EditText(this);
+        AuthCredential credential =  EmailAuthProvider
+                .getCredential("doesnt matter", "wont work by default");
+
+        Context context = textView.getContext();
+        LinearLayout layout = new LinearLayout(context);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.addView(emailText);
+        layout.addView(passwordText);
+        layout.addView(newEmailText);
+        dialog.setView(layout); // Again this is a set method, not add
+        dialog.setTitle("Change Email");
+        dialog.setButton(DialogInterface.BUTTON_POSITIVE, "Change email",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ReauthenticateAndChangeEmail(emailText.getText().toString(),
+                                passwordText.getText().toString(),
+                                newEmailText.getText().toString());
+                    }
+                });
+
+        dialog.show();
     }
+
     private void ReauthenticateAndChangePassword(String email, String curPassword, String newPassword) {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         AuthCredential credential = EmailAuthProvider
                 .getCredential(email, curPassword);
-        user.reauthenticate(credential)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(getApplicationContext(), "Successfully change password", Toast.LENGTH_SHORT).show();
-                                        Log.d("resetpassTag", "Password updated");
-                                    } else {
-                                        Log.d("resetpassTag", "Error password not updated");
-                                    }
-                                }
-                            });
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Email or password incorrect", Toast.LENGTH_SHORT).show();
-                            Log.d("resetpassTag", "Error auth failed");
+        user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(Task<Void> task) {
+                if (task.isSuccessful()) {
+                    user.updatePassword(newPassword).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Successfully changed password",
+                                        Toast.LENGTH_SHORT).show();
+                                Log.d("resetpassTag", "Password updated");
+                            } else {
+                                Log.d("resetpassTag", "Error password not updated");
+                            }
                         }
-                    }
-                });
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "Email or password incorrect",
+                            Toast.LENGTH_SHORT).show();
+                    Log.d("resetpassTag", "Error auth failed");
+                }
+            }
+        });
 
+    }
+
+    private void ReauthenticateAndChangeEmail(String email, String curPassword, String newEmail) {
+        /*FirebaseUser authUser = FirebaseAuth.getInstance().getCurrentUser();
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(email, curPassword);
+        authUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(Task<Void> task) {
+                if (task.isSuccessful()) {
+                    authUser.updateEmail(newEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "Successfully changed email",
+                                        Toast.LENGTH_SHORT).show();
+                                //DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+                                //rootRef.child("users").
+                                setUpTextFields();
+                                Log.d("resetpassTag", "Email updated");
+                            } else {
+                                Log.d("resetpassTag", "Error email not updated");
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "Email or password incorrect",
+                            Toast.LENGTH_SHORT).show();
+                    Log.d("resetpassTag", "Error auth failed");
+                }
+            }
+        });*/
     }
 }
