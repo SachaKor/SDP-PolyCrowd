f2ef778f5802ba8c0c657ed5b0af1ef4c1c71a73
yabdro <hafez435@gmail.com>
2020-04-30 18:53:41 +0200

    provides public calls to highlight user markers on map, and resetmap

--- app/src/main/java/ch/epfl/polycrowd/groupPage/GroupMapFragment.java
+++ app/src/main/java/ch/epfl/polycrowd/groupPage/GroupMapFragment.java
@@ -18,24 +18,26 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import ch.epfl.polycrowd.R;
 import ch.epfl.polycrowd.logic.Group;
 import ch.epfl.polycrowd.logic.PolyContext;
 import ch.epfl.polycrowd.logic.User;
 
-public class GroupMapFragment extends Fragment implements OnMapReadyCallback {
+public class GroupMapFragment extends Fragment implements OnMapReadyCallback  {
 
+    private static final String TAG = "GroupMapFragment";
     private static GroupMapFragment INSTANCE = null ;
     private GoogleMap googleMap ;
     private MapView mapView ;
 
-    View view ;
+    private Map<User, LatLng> memberLocationsMap ;
+    private Map<User, MarkerOptions> memberMarkersMap;
 
     public static GroupMapFragment getINSTANCE(){
         if(INSTANCE == null)
@@ -51,7 +53,7 @@ public class GroupMapFragment extends Fragment implements OnMapReadyCallback {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_group_map, container, false) ;
+        View view = inflater.inflate(R.layout.fragment_group_map, container, false) ;
         return view ;
     }
 
@@ -64,45 +66,61 @@ public class GroupMapFragment extends Fragment implements OnMapReadyCallback {
             mapView.onResume();
             mapView.getMapAsync(this);
         }
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        Log.d("GroupMapFragment", "RIGHT BEFORE CALL TO MapsInitializer.initialize") ;
         MapsInitializer.initialize(getContext()) ;
         this.googleMap = googleMap ;
-        List<LatLng> memberLocations = getGroupMemberPositions() ;
 
+        initMemberLocationsAndMarkers();
+        //Add markers
+        memberMarkersMap.values().forEach(markerOptions -> googleMap.addMarker(markerOptions));
+
+        //Fit map over users' locations
         LatLngBounds.Builder boundsBuilder  = LatLngBounds.builder() ;
-
-        for(LatLng location: memberLocations) {
-            googleMap.addMarker(new MarkerOptions().position(location).title("a friend is here"));
-            boundsBuilder.include(location) ;
-        }
-
-        List<Marker> locationMarkers = new LinkedList<>() ;
-        for(int i = 0 ; i < locationMarkers.size() ; ++i) {
-            MarkerOptions currentLocationMarkerOptions = new MarkerOptions().position(memberLocations.get(i)).icon(BitmapDescriptorFactory.fromResource(R.drawable.pointred));
-            locationMarkers.set(i, googleMap.addMarker(currentLocationMarkerOptions));
-        }
-
+        memberLocationsMap.values().forEach(l -> boundsBuilder.include(l));
         LatLngBounds bounds = boundsBuilder.build() ;
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bounds.getCenter(), 20));
-        //googleMap.fitBounds(bounds) ;
-
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 0));
     }
 
-    private List<LatLng> getGroupMemberPositions(){
-        List<LatLng> l = new LinkedList<>();
-        /*l.add(new LatLng(46.518033, 6.566919));l.add(new LatLng(46.518933, 6.566819));l.add(new LatLng(46.518533, 6.566719));
-        l.add(new LatLng(46.518333, 6.566119));l.add(new LatLng(46.518033, 6.566319));l.add(new LatLng(46.518933, 6.566419));
-        l.add(new LatLng(46.518733, 6.566519));l.add(new LatLng(46.518033, 6.566619));l.add(new LatLng(46.518633, 6.566719));
-        l.add(new LatLng(46.518533, 6.566819));l.add(new LatLng(46.518333, 6.566319));l.add(new LatLng(46.518233, 6.566419));*/
+    public void highlightUserMarker(User user){
+        Log.d(TAG, "HIGHLIGHT USER MARKER") ;
+        MarkerOptions markerOptions = memberMarkersMap.get(user) ;
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        drawMap();
+    }
+
+    private void initMemberLocationsAndMarkers(){
+        memberLocationsMap = new HashMap<>();
+        memberMarkersMap = new HashMap<>();
         Group gr = PolyContext.getCurrentGroup() ;
         for(User u: gr.getMembers()){
-            l.add(u.getLocation()) ;
+            LatLng location = u.getLocation() ;
+            memberLocationsMap.put(u, location ) ;
+            memberMarkersMap.put(u, new MarkerOptions().position(location).title(u.getName()).icon(BitmapDescriptorFactory.fromResource(R.drawable.pointred))) ;
         }
-        return l;
+    }
+
+    private LatLngBounds getBounds(Collection<LatLng> locations){
+        LatLngBounds.Builder boundsBuilder  = LatLngBounds.builder() ;
+        memberLocationsMap.values().forEach(l -> boundsBuilder.include(l));
+        return  boundsBuilder.build() ;
+    }
+
+    private void drawMap() {
+        if(googleMap != null) {
+            googleMap.clear();
+            //TODO need to also update the member locations
+            memberMarkersMap.values().forEach(mo -> googleMap.addMarker(mo));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(getBounds(memberLocationsMap.values()), 0));
+        }
+    }
+
+    public void resetMap() {
+        if ( memberMarkersMap != null){
+            memberMarkersMap.values().forEach(markerOptions -> markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.pointred)));
+            drawMap();
+        }
     }
 }
