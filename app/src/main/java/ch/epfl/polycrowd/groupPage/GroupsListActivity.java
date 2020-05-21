f16aed0106f890a152dcb92c18285aef60fe1fd5
yabdro <hafez435@gmail.com>
2020-05-22 01:08:22 +0200

    extend creategroupdialogfragment listener and hides button if current event is null

--- app/src/main/java/ch/epfl/polycrowd/groupPage/GroupsListActivity.java
+++ app/src/main/java/ch/epfl/polycrowd/groupPage/GroupsListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,7 +28,7 @@ import ch.epfl.polycrowd.userProfile.ItemClickListener;
 
 import static ch.epfl.polycrowd.ActivityHelper.toastPopup;
 
-public class GroupsListActivity extends AppCompatActivity {
+public class GroupsListActivity extends AppCompatActivity  implements CreateGroupDialogFragment.CreateGroupDialogListener  {
 
     private static final String TAG = "GroupsListActivity" ;
 
@@ -39,12 +40,56 @@ public class GroupsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups_list);
 
+        //---- Set create group button visiblity
+        Button createGroupButton = findViewById(R.id.createGroupButton) ;
+        if(PolyContext.getCurrentEvent() != null){
+            createGroupButton.setVisibility(View.VISIBLE);
+            createGroupButton.setOnClickListener(v -> onCreateGroupClicked(v) );
+        } else{
+            // Even if the button is invisible, might react to clicks, so does nothing in listener
+            createGroupButton.setOnClickListener(v -> {});
+        }
+
         // ---- setup adapter and recycler view --------------------------------------------------
         List<Group> groupList  = PolyContext.getUserGroups() ;
         RecyclerView mRecyclerView = findViewById(R.id.groupsListRecyclerView);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mAdapter = new GroupsListAdapter(groupList, this) ;
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public void onCreateGroupClicked(View view) {
+        showNoticeDialog();
+    }
+
+    private void showNoticeDialog() {
+        // Create an instance of the dialog fragment and show it
+        DialogFragment dialog = new CreateGroupDialogFragment();
+        dialog.show(getSupportFragmentManager(), "CreateGroupDialogFragment");
+    }
+
+    @Override
+    public void onOKCreateGroupClick(DialogFragment dialog, String groupName) {
+
+        if(groupName == null || groupName.isEmpty())
+            return ;
+        //TODO does it make a difference whether this user set is initialized inside of the callback or not?
+        //What if the user logs out before the callback's been executed?
+        Set<User> memberSet = new HashSet<>() ;
+        memberSet.add(PolyContext.getCurrentUser()) ;
+
+        //Toasts for error handling
+        Toast generalErrorToast = Toast.makeText(this, "Error creating group, try again later", Toast.LENGTH_LONG) ;
+        Toast createGroupSuccessToast = Toast.makeText(this, groupName+" created successfully!", Toast.LENGTH_LONG) ;
+        //Setup new group
+        Group group = new Group(groupName, PolyContext.getCurrentEvent().getName(), PolyContext.getCurrentEvent().getId(), memberSet) ;
+        PolyContext.getDBI().createGroup(group.getRawData(), groupId -> {
+            group.setGid(groupId);
+            createGroupSuccessToast.show();
+            PolyContext.getUserGroups().add(group) ;
+            mAdapter.notifyAdapterDatasetChanged(PolyContext.getUserGroups());
+        });
+
     }
 
     private class GroupsListAdapter extends RecyclerView.Adapter<GroupsListHolder> {
