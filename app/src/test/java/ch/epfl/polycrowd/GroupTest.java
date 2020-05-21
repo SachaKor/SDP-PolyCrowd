ce32211bcf0abc2abe26d0390a52875a374e36c8
yabdro <hafez435@gmail.com>
2020-05-21 23:59:43 +0200

    unit tests for groups

--- app/src/test/java/ch/epfl/polycrowd/GroupTest.java
+++ app/src/test/java/ch/epfl/polycrowd/GroupTest.java
@@ -3,9 +3,12 @@ package ch.epfl.polycrowd;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import ch.epfl.polycrowd.logic.Group;
 import ch.epfl.polycrowd.logic.User;
@@ -18,32 +21,53 @@ public class GroupTest {
     String userEmail = "newuser@mail.com" ; 
     String username = "newUser" ; 
     String eventName = "newevent" ; 
-    String eventId = "neweventId1" ; 
+    String eventId = "neweventId1" ;
+    Set<User> members ;
+    Long age = 20L ;
     
     @Before
     public void userAndGroupSetup(){
         user = new User( userEmail, "1", username,  20, null) ;
-        group = new Group(groupName, eventName,eventId, new HashSet<>()) ;
-        group.addMember(user);
-    }
-
-         //   m.put("groupName", groupName) ;
-        //m.put("eventName", eventName);
-        //m.put("eventId", eventId) ;
-        //m.put("members", new ArrayList<>(members));
-    @Test
-    public void getFromDocumentConvertsUsers(){
-
+        members = new HashSet<>() ;
+        members.add(user) ;
+        group = new Group(groupName, eventName,eventId, members) ;
     }
 
     @Test
-    public void getRawDataWithgetFromDocumentMatches(){
+    public void getFromDocumentReturnsMatchingGroup(){
+        Map<String, Object> document = new HashMap<>() ;
+        List<Map<String, Object>> usersRawData = new ArrayList<>() ;
+        Map<String, Object> userRd = user.getRawData();
+        userRd.put("age", 20L);
+        userRd.put(User.uidTag, "1") ;
+        usersRawData.add(userRd) ;
+        document.put("members", usersRawData) ;
+        document.put("groupName", groupName) ;
+        document.put("eventName", eventName) ;
+        document.put("eventId", eventId) ;
+        Group groupFromDocument = Group.getFromDocument(document) ;
+        assert(groupFromDocument.getGroupName().equals(groupName));
+        User userFromDocument = (User) groupFromDocument.getMembers().toArray()[0] ;
+        assert(userFromDocument.getEmail().equals(userEmail)) ;
+        assert(userFromDocument.getUid().equals("1")) ;
+        assert(userFromDocument.getUsername().equals(username)) ;
+        assert(groupFromDocument.getEventName().equals(eventName)) ;
+        assert(groupFromDocument.getEventId().equals(eventId)) ;
+    }
+
+    @Test
+    public void getRawDataMatchesOriginalData(){
         Map<String, Object> groupRawData =  group.getRawData() ;
         assert(groupRawData.get("groupName").toString() == groupName) ; 
         assert(groupRawData.get("eventName").toString() == eventName) ; 
         assert(groupRawData.get("eventId").toString() == eventId ) ; 
-        List<User> members =  (List<User>)groupRawData.get("members") ;
-        assert( members.contains(user)) ;
+        List<Map<String, Object>> membersRawData =  (List<Map<String, Object>>)groupRawData.get("members") ;
+        Map<String, Object> userRawData = membersRawData.get(0) ;
+        assert(userRawData.get(User.emailTag).toString().equals(userEmail)) ;
+        assert(userRawData.get(User.userNameTag).toString().equals(username)) ;
+        assert(userRawData.get(User.ageTag).toString().equals(age.toString())) ;
+        assert (userRawData.get(User.imageUriTag) == null) ;
     }
+
     
 }
