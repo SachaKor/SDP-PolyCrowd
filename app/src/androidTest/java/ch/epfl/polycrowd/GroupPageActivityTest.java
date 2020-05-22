d71aa849b29424406161ef5407ce84e4dd9b5ec3
yabdro <hafez435@gmail.com>
2020-05-22 04:50:26 +0200

    tests goes to map activity click on go-to-map click

--- app/src/androidTest/java/ch/epfl/polycrowd/GroupPageActivityTest.java
+++ app/src/androidTest/java/ch/epfl/polycrowd/GroupPageActivityTest.java
@@ -3,6 +3,7 @@ package ch.epfl.polycrowd;
 import android.content.Intent;
 
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Before;
@@ -14,13 +15,21 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import ch.epfl.polycrowd.groupPage.GroupPageActivity;
+import ch.epfl.polycrowd.logic.Event;
 import ch.epfl.polycrowd.logic.Group;
 import ch.epfl.polycrowd.logic.PolyContext;
 import ch.epfl.polycrowd.logic.User;
+import ch.epfl.polycrowd.map.MapActivity;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItem;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 public class GroupPageActivityTest {
@@ -31,6 +40,7 @@ public class GroupPageActivityTest {
 
     Group group0 ;
     User testUser0;
+    Event event ;
 
     private final String SECOND_FRAG_TITLE = "SECONDSEGMENT" ;
     private final String FIRST_FRAG_TITLE =   "FIRSTSEGMENT" ;
@@ -44,6 +54,8 @@ public class GroupPageActivityTest {
         Group group0 = new Group("testGid0", "testEvent" ,"testEventId0", new HashSet<>()) ;
         group0.addMember(testUser0);
         PolyContext.getDBI().createGroup(group0.getRawData(), gr -> {
+            event = AndroidTestHelper.getDebugEvent() ;
+            PolyContext.setCurrentEvent(event);
             PolyContext.setCurrentGroup(group0);
             Intent intent = new Intent() ;
             groupPageActivityRule.launchActivity(intent) ;
@@ -53,44 +65,24 @@ public class GroupPageActivityTest {
 
     @Test
     public void userListShowsCorrectlyMembersOfGroup(){
-        //First navigate to user-list fragment
-        // navigateToFragment(false);
         onView(withText(testUser0.getEmail())).check(matches((isDisplayed()))) ;
         onView(withText(testUser0.getUsername())).check(matches((isDisplayed()))) ;
     }
 
-    /*@Test
-    public void switchesToMapFragmentOnUserClick(){
-        navigateToFragment(false);
-        AndroidTestHelper.sleep();
-        try {
-            sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        onView(withText(testUser0.getEmail())).perform(ViewActions.click()) ;
-        AndroidTestHelper.sleep();
-        try {
-            sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        onView(withText(testUser0.getEmail())).check(matches(not(isDisplayed()))) ;
-        onView(withText(testUser1.getEmail())).check(matches(not(isDisplayed()))) ;
-        onView(withText(testUser0.getName())).check(matches(not(isDisplayed()))) ;
-        onView(withText(testUser1.getName())).check(matches(not(isDisplayed()))) ;
-    }*/
+     @Test
+     public void onUserClickNothingHappens(){
+         onView(withId(R.id.members_recycler_view)).
+                 perform(actionOnItem(hasDescendant(withText(testUser0.getUsername())), click()));
+     }
 
-    private void navigateToFragment(boolean isMapFrag){
-        assert(PolyContext.getCurrentGroup() == group0) ;
-        assert(PolyContext.getCurrentUser() == testUser0) ;
-        for(int i : Arrays.asList(1 ,23, 4, 5, 5))
-            AndroidTestHelper.sleep();
 
-        if(isMapFrag){
-            onView(withText(FIRST_FRAG_TITLE)).perform(ViewActions.click()) ;
-        } else{
-            onView(withText(SECOND_FRAG_TITLE)).perform(ViewActions.click()) ;
-        }
-    }
+     @Test
+     public void goesToMapOnGoToMapClick(){
+         Intents.init() ;
+         onView(withId(R.id.go_to_map_button)).
+                 perform(click());
+         intended(hasComponent(MapActivity.class.getName())) ;
+     }
+
+
 }
